774b56049023232f74532f339bc956be39d1dcac
ocece77 <oceanekasindupro@gmail.com>
2025-04-14 16:47:06 +0200

    intégration de la hero section

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const HeaderComponent = ({titre, desc, btns}) => {
   return (
@@ -15,14 +16,14 @@ const HeaderComponent = ({titre, desc, btns}) => {
 
             {/*Boutons d'action */}
             <div className='flex  w-full '>
-              <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-primary rounded-lg bg-white hover:bg-red-800 hover:text-white transition-all">
+              <Link to="contacts" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-primary rounded-lg bg-white hover:bg-red-800 hover:text-white transition-all">
                     Nous contacter
                   <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-              </a>
-              <a href="#" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-red-800 hover:text-white transition-all">
+              </Link>
+              <Link to="about" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary hover:bg-red-800 hover:text-white transition-all">
                   Nous découvrir
                   <svg className="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-              </a> 
+              </Link> 
 
             </div>
 
