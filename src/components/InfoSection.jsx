3e5bcee7ece82a980f8932766177621a88d59c87
Ocece77 <oceanekasindupro@gmail.com>
2025-04-16 12:16:58 +0200

    création de la section info

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,10 +1,51 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const InfoSection = ({img , subtitre , titre , desc , btn})  => {
+const InfoSection = ({img , subtitre , titre , desc , btn , reversed=false})  => {
   return (
-    <div className=' h-[70vh] w-screen bg-amber-500 px-50'>
-      <div className='grid grid-cols-2'>
+    <div className=' h-full w-screen px-5 md:px-20 py-10'>
+      <div className='grid md:grid-cols-2 h-full md:gap-10'>
+          {/*col de gauche */}
+          <div >
+            <img src={img} alt={`image section - ${titre}`} className="w-full h-full lg:w-3/4 object-cover"/>
+          </div>
+
+         {/*col de droite */}
+          <div className="flex flex-col gap-5 justify-center">
+            {/*titre*/}
+            {
+              subtitre &&  (
+                <p className="font-bold text-red-800">
+                {titre}
+              </p>
+              )
+            }
+           
+
+            {/*titre*/}
+            <h1 className="lg:text-6xl md:text-3xl text-xl font-bold text-primary">
+              {titre}
+            </h1>
+
+            {/*description*/}
+            <p className="lg:text-base text-[12px] text-dark-primary ">
+             {desc}
+            </p>
+
+            {/*btn*/}
+            {
+              btn &&
+              (
+                <Link to={btn.btnLink} className="inline-flex items-center justify-center max-w-fit px-5 py-2 mr-3 text-sm font-medium text-center rounded-lg bg-red-800 text-white transition-all ">
+                 {btn.btnTitre}
+                </Link>
+              )
+            }
+          
+
+          </div>
+
 
       </div>
     </div>
@@ -19,7 +60,8 @@ InfoSection.propTypes = {
   btn : PropTypes.shape({
     btnTitre : PropTypes.string.isRequired,
     btnLink : PropTypes.string.isRequired
-  })
+  }),
+  reversed:  PropTypes.bool.isRequired,
 }
 
 export default InfoSection
