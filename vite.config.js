a270498598a1a7140528fb6e17b9de1b67184ffc
ocece77 <oceanekasindupro@gmail.com>
2026-08-24 19:14:02 +0200

    installation de flow bite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import flowbiteReact from "flowbite-react/plugin/vite";
 import tailwindcss from '@tailwindcss/vite'
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), flowbiteReact(), tailwindcss()],
+  plugins: [react(), tailwindcss(), flowbiteReact()],
   base: "https://github.com/Ocece77/fidex_distribution",
 
 
